9719f3655850095c0120181fe183c3c1742fcb63
Gaël Écorchard <gael.ecorchard@cvut.cz>
2022-03-21 17:49:08 +0100

    [telescope] Improve format of long files

--- lua/plugin_setup/telescope.lua
+++ lua/plugin_setup/telescope.lua
@@ -1,5 +1,15 @@
 require('telescope').setup({
 
+  defaults = {
+    -- path_display = {
+    --   shorten = 1,  -- Shorten directory names leaving 1 letter.
+    -- },
+    path_display = function(opts, path)
+      local tail = require('telescope.utils').path_tail(path)
+      return string.format("%s (%s)", tail, path)
+    end,
+  },
+
   pickers = {
     find_files = {
       theme = "dropdown",
