34b1dc239325a4b2c6799be1b105f79403eea46d
Gaël Écorchard <gael.ecorchard@cvut.cz>
2023-03-23 09:18:14 +0100

    [treesitter-context] Remove deprecated patterns
    Configuration is now done via treesitter queries.

--- lua/plugin_setup/treesitter_context.lua
+++ lua/plugin_setup/treesitter_context.lua
@@ -1,23 +1,9 @@
+-- treesitter-context shows the context (e.g. name of the function we are
+-- currently in) at the top of the screen.
+
 require('treesitter-context').setup{
     enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
     max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
     trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
     min_window_height = 20, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
-    patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
-        -- For all filetypes
-        -- Note that setting an entry here replaces all other patterns for this entry.
-        -- Patterns for specific filetypes
-        -- If a pattern is missing, *open a PR* so everyone can benefit.
-        cpp = {
-          'access_specifier',
-          'case',
-          'class',
-          'for',
-          'function',
-          'if',
-          'method',
-          'switch',
-          'while',
-        },
-    },
 }
