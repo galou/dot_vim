d06fc287830e3e47c03a604b899a8e887b9acfb8
Gaël Écorchard <gael.ecorchard@cvut.cz>
2023-07-25 14:52:21 +0200

    [clangd_extensions] Fix configuration

--- lua/plugin_setup/clangd_extensions.lua
+++ lua/plugin_setup/clangd_extensions.lua
@@ -47,23 +47,26 @@ require('clangd_extensions').setup {
     extensions = {
         -- defaults:
         -- Automatically set inlay hints (type hints)
-        autoSetHints = true,
+        autoSetHints = vim.fn.has('nvim-0.10') ~= 1,
         -- These apply to the default ClangdSetInlayHints command
         inlay_hints = {
+            inline = vim.fn.has('nvim-0.10') == 1,
+            -- Options other than `highlight' and `priority' only work
+            -- if `inline' is disabled
             -- Only show inlay hints for the current line
-            only_current_line = true,
-            -- Event which triggers a refresh of the inlay hints.
-            -- You can make this "CursorMoved" or "CursorMoved,CursorMovedI" but
+            only_current_line = false,
+            -- Event which triggers a refersh of the inlay hints.
+            -- You can make this 'CursorMoved' or 'CursorMoved,CursorMovedI' but
             -- not that this may cause  higher CPU usage.
             -- This option is only respected when only_current_line and
             -- autoSetHints both are true.
-            only_current_line_autocmd = "CursorHold,CursorMoved",
+            only_current_line_autocmd = 'CursorHold',
             -- whether to show parameter hints with the inlay hints or not
             show_parameter_hints = true,
             -- prefix for parameter hints
-            parameter_hints_prefix = " ",
+            parameter_hints_prefix = ' ',
             -- prefix for all the other hints (type, chaining)
-            other_hints_prefix = "⟹ ",
+            other_hints_prefix = '⟹ ',
             -- whether to align to the length of the longest line in the file
             max_len_align = false,
             -- padding from the left if max_len_align is true
@@ -73,60 +76,59 @@ require('clangd_extensions').setup {
             -- padding from the right if right_align is true
             right_align_padding = 7,
             -- The color of the hints
-            highlight = "Comment",
+            highlight = 'Comment',
             -- The highlight group priority for extmark
             priority = 100,
         },
         ast = {
-            -- These are unicode, should be available in any font
-            --[[
-            role_icons = {
-                 type = "🄣",
-                 declaration = "🄓",
-                 expression = "🄔",
-                 statement = ";",
-                 specifier = "🄢",
-                 ["template argument"] = "🆃",
-            },
-            kind_icons = {
-                Compound = "🄲",
-                Recovery = "🅁",
-                TranslationUnit = "🅄",
-                PackExpansion = "🄿",
-                TemplateTypeParm = "🅃",
-                TemplateTemplateParm = "🅃",
-                TemplateParamObject = "🅃",
-            },
-            ]]
+            --[[ These are unicode, should be available in any font
+            -- role_icons = {
+            --      type = '🄣',
+            --      declaration = '🄓',
+            --      expression = '🄔',
+            --      statement = ';',
+            --      specifier = '🄢',
+            --      ['template argument'] = '🆃',
+            -- },
+            -- kind_icons = {
+            --     Compound = '🄲',
+            --     Recovery = '🅁',
+            --     TranslationUnit = '🅄',
+            --     PackExpansion = '🄿',
+            --     TemplateTypeParm = '🅃',
+            --     TemplateTemplateParm = '🅃',
+            --     TemplateParamObject = '🅃',
+            -- },
+            --]]
             -- These require codicons (https://github.com/microsoft/vscode-codicons)
             role_icons = {
-                type = "",
-                declaration = "",
-                expression = "",
-                specifier = "",
-                statement = "",
-                ["template argument"] = "",
+                type = '',
+                declaration = '',
+                expression = '',
+                specifier = '',
+                statement = '',
+                ['template argument'] = '',
             },
 
             kind_icons = {
-                Compound = "",
-                Recovery = "",
-                TranslationUnit = "",
-                PackExpansion = "",
-                TemplateTypeParm = "",
-                TemplateTemplateParm = "",
-                TemplateParamObject = "",
+                Compound = '',
+                Recovery = '',
+                TranslationUnit = '',
+                PackExpansion = '',
+                TemplateTypeParm = '',
+                TemplateTemplateParm = '',
+                TemplateParamObject = '',
             },
 
             highlights = {
-                detail = "Comment",
+                detail = 'Comment',
             },
         },
         memory_usage = {
-            border = "none",
+            border = 'none',
         },
         symbol_info = {
-            border = "none",
+            border = 'none',
         },
     },
 }
